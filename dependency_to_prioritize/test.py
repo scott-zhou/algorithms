c5ab1a4c0fec21023869ad49daff783b25fbe4a3
Scott Zhou <scott.cong.zhou@ericsson.com>
2015-11-25 16:34:55 +0100

    Add expected result in test code.

--- dependency_to_prioritize/test.py
+++ dependency_to_prioritize/test.py
@@ -16,6 +16,15 @@ deps = set(
 p = Prioritize()
 p.convertFrom(deps)
 print(p._priorityLevel)
+"""
+Expected result:
+a: 0
+d: 1
+b: 2
+c/g: 3
+f: 4
+e: 5
+"""
 
 deps = set(
     [('a', 'b'),
@@ -28,10 +37,23 @@ deps = set(
      ])
 p.convertFrom(deps)
 print(p._priorityLevel)
+"""
+Expected result:
+f: 0
+e: 1
+c/d: 2
+b/g: 3
+a: 4
+"""
 
 deps = set([('a', 'b')])
 p.convertFrom(deps)
 print(p._priorityLevel)
+"""
+Expected result:
+b: 0
+a: 1
+"""
 
 deps = set(
     [('a', 'b'),
@@ -40,3 +62,8 @@ deps = set(
      ])
 p.convertFrom(deps)
 print(p._priorityLevel)
+"""
+Expected result:
+ERROR
+"""
+
